5f1a24746c98b810805005659b3a7811a02b5a4a
PaddyWang <paddy.wang@cn.feiboedu.com>
2018-11-22 19:16:51 +0800

    add media-screen

---
+++ js/init-canvas-background.js
@@ -0,0 +1,20 @@
+;(function(){
+    var canvasBackgroundInstance = new CanvasBackground(initConfig());
+
+    window.onresize = function(){
+        canvasBackgroundInstance.reset(initConfig());
+    };
+
+    function initConfig(){
+        var baseW = 1920;
+        var fontSize = window.innerWidth / baseW * 32;
+        var xGap = window.innerWidth / baseW * 200;
+        var yGap = window.innerWidth / baseW * 200;
+        return {
+            fontSize: fontSize > 16 ? fontSize : 16,
+            angle: -35,
+            xGap: xGap > 80 ? xGap : 80,
+            yGap: yGap > 80 ? yGap : 80
+        };
+    }
+}());
